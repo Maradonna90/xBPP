f5b99230732760ac516d3dbde2f54474fc5f6550
Maradonna <marco.jendryczko@gmx.net>
2020-03-30 19:03:08 +0200

    predict boosting and tables

--- predict_boosting.py
+++ predict_boosting.py
@@ -4,27 +4,27 @@ from joblib import dump, load
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-store = pd.HDFStore('pitches.h5')
+store = pd.HDFStore('data/pitches.h5')
 store_keys = store.keys()
 store.close()
-lgbm = load('lgbm.joblib')
-cat = load('cb.joblib')
-models = [lgbm, cat]
-names = ['lgbm', 'cat']
+lgbm = load('models/lgbm.joblib')
+cat = load('models/cb.joblib')
+xgb = load('models/xgb.joblib')
+models = [lgbm, cat, xgb]
+names = ['lgbm', 'cb', 'xgb']
 data_columns = ['release_speed', 'release_pos_x', 'release_pos_z', 'release_pos_y', 'pfx_x', 'pfx_z', 'plate_x', 'plate_z', 'release_spin_rate']
 for key in store_keys:
     print(key)
-    data = pd.read_hdf('pitches.h5', key)
-    #else:
-    #    data = pd.read_hdf('pitches.h5', key, start=2)
+    data = pd.read_hdf('data/pitches.h5', key)
+    print(data.shape)
     X = data.loc[:, data_columns].values
     sc = StandardScaler()
     x = sc.fit_transform(X)
     for model, name in zip(models, names):
         prediction = model.predict(x)
         data.loc[:, name] = prediction
-    data.loc[:,"xBPP"] = data.loc[:, "lgbm"] * 0.5 + data.loc[:, "cat"] * 0.5
+        data.loc[:,"xBPP"] = data.loc[:, "lgbm"] * 0.5 + data.loc[:, "xgb"] * 0.3 + data.loc[:, 'cb'] * 0.2
     if key == '/data0':
-        data.to_csv("combined_xBPP.csv", index=False, mode='a')
+        data.to_csv("results/combined_xBPP.csv", index=False, mode='a')
     else:
-         data.to_csv("combined_xBPP.csv", index=False, mode='a', header=False)
+         data.to_csv("results/combined_xBPP.csv", index=False, mode='a', header=False)
